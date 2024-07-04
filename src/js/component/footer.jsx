854618c25e48f84eb6ff676465a241e5d13aaea0
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-04 21:00:30 +0000

    1era subida

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+      <div className="position-absolute bottom-0 w-100 text-center text-light bg-dark">
+        <p className='fs-6 fw-light'>Copywrigth your web site 2024</p>
+      </div>
+  );
+}
+
+export default Footer;
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron jumbotron-fluid bg-light mb-2 p-3 pb-5">
+      <div className="container text-left">
+        <h1 className="display-4 fw-bolder">A Warm Welcome!</h1>
+        <p>
+            Lorem ipsum is the way to go if you think about it just for a little while little guy.
+            Lorem ipsum is the way to go if you think about it just for a little while little guy.
+            Lorem ipsum is the way to go if you think about it just for a little while little guy.
+        </p>
+        <button className="btn btn-primary" type="submit">Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
